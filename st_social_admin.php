17773596f9a515208b92d646cd140b26e2497914
dharashah <dharashah@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-19 08:36:08 +0000

    Compatible with 3.9
    git-svn-id: https://plugins.svn.wordpress.org/st-social-links/tags/0.0.5@897944 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ st_social_admin.php
@@ -0,0 +1,314 @@
+<?php
+if( ! class_exists( 'WP_List_Table' ) ) {
+    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+
+$networks = array('facebook' => 'Facebook',
+                 'twitter'=> 'Twitter',
+				 'googleplus'=>'Google Plus');
+				 
+$icon_images = array("facebook"  => array("facebook_30","facebook_32","facebook_48","facebook_48_grey","facebook_64"),
+                     "twitter"   => array("twitter_30","twitter_32","twitter_48","twitter_48_grey","twitter_64"),
+					 "googleplus" => array("googleplus_30","googleplus_32","googleplus_48","googleplus_48_grey","googleplus_64"));
+
+add_action('admin_menu', 'social_link_admin_menu');
+function social_link_admin_menu() 
+{
+	$page = add_menu_page( 'Social Link Page', 'Social Link', 'manage_options','social-link','social_link_option_page', plugins_url( 'st-social-links/images/icon.png' ));
+	wp_register_style('stylesocial.css',WP_SOCIAL_LINK_URL.'/css/stylesocial.css');
+	wp_enqueue_style('stylesocial.css');
+}
+
+
+class Social_Link_List_Table extends WP_List_Table {
+	function get_columns(){
+		$columns = array(
+			'social_network' 	=> 'Network',
+			'social_link_url'   => 'Link',
+			'img_link'      	=> 'Image'
+		);
+		return $columns;
+	}
+	function get_sortable_columns() {
+		$sortable_columns = array(
+			'social_network' => array('social_network',false),
+			'social_link_url' 	=> array('social_link_url',false)
+		);
+		return $sortable_columns;
+	}
+	function usort_reorder( $a, $b ) {
+		// If no sort, default to title
+		$orderby = ( ! empty( $_GET['orderby'] ) ) ? $_GET['orderby'] : 'social_network';
+		// If no order, default to asc
+		$order = ( ! empty($_GET['order'] ) ) ? $_GET['order'] : 'asc';
+		// Determine sort order
+		$result = strcmp( $a[$orderby], $b[$orderby] );
+		// Send final sort direction to usort
+		return ( $order === 'asc' ) ? $result : -$result;
+	}
+	function prepare_items() {
+		$columns = $this->get_columns();
+		$hidden = array();
+		$sortable = $this->get_sortable_columns();
+		
+		global $wpdb;
+		global $table_suffix;
+		$table_suffix = "sociallink";
+		$table_name = $wpdb->prefix . $table_suffix;
+		$this->items = $wpdb->get_results("SELECT * FROM $table_name",ARRAY_A);
+		$this->_column_headers = array($columns, $hidden, $sortable);
+		usort( $this->items, array( &$this, 'usort_reorder' ) );
+		
+		$per_page = 5;
+		$current_page = $this->get_pagenum();
+		$total_items = count($this->items);
+		// only ncessary because we have sample data
+			$this->found_data = array_slice($this->items,(($current_page-1)*$per_page),$per_page);
+			$this->set_pagination_args( array(
+				'total_items' => $total_items,                  //WE have to calculate the total number of items
+				'per_page'    => $per_page                     //WE have to determine how many items to show on a page
+			) );
+		$this->items = $this->found_data;
+	}
+	function column_default( $item, $column_name ) {
+	  switch( $column_name ) { 
+		case 'social_network':
+			return $item[ $column_name ];
+		case 'social_link_url':
+			return '<a href="' . $item[ $column_name ] . '" >' . $item[ $column_name ] . '</a>';
+		case 'img_link': 
+			return '<img src="' . $item[ $column_name ] . '" />';
+		default:
+		  return print_r( $item, true ) ; //Show the whole array for troubleshooting purposes
+	  }
+	}
+	function process_bulk_action() {
+        
+        //Detect when a bulk action is being triggered...
+        if( 'delete'===$this->current_action() ) {
+            wp_die('Items deleted (or they would be if we had items to delete)!');
+        }
+        
+    }
+	function column_social_network($item) {
+		$actions = array(
+            'edit'      => sprintf('<a href="?page=%s&action=%s&social_link=%s">Edit</a>',$_REQUEST['page'],'edit',$item['id']),
+            'delete'    => sprintf('<a href="?page=%s&action=%s&social_link=%s">Delete</a>',$_REQUEST['page'],'delete',$item['id']),
+        );
+		return sprintf('%1$s %2$s', $item['social_network'], $this->row_actions($actions) );
+	}
+	function column_cb($item) {
+        return sprintf(
+            '<input type="checkbox" name="social_link[]" value="%s" />', $item['ID']
+        );    
+    }
+	function no_items() {
+		_e( 'Add a Social Link using the form above.' );
+	}
+}
+
+
+
+function set_option($option_name,$new_value)
+{
+	//option exists?
+	if ( !get_option( $option_name )) {
+		//add if not exists
+		add_option( $option_name, $new_value);
+	} else {
+		//update if exists
+		update_option( $option_name, $new_value );
+	}
+}
+
+?>
+<?php
+function social_link_option_page() {
+	global $networks;
+	
+	?>
+	<script>
+		function showimages(){
+			var social_network = document.getElementById("social_network").value;
+			//first hide all
+			<?php
+				foreach($networks as $k => $v)
+				{	echo "document.getElementById('".$k."').style.display='none';";	}
+			?>
+			//show the required div
+  		    document.getElementById(social_network).style.display="block";
+		}
+	</script>
+	<?php
+		$social_network = $_REQUEST["social_network"];
+		$social_link_url = $_REQUEST["social_link_url"];
+		$img_link = plugins_url('images/'. $_REQUEST["img_link"] .'.png',__FILE__);
+		
+		global $wpdb;
+		global $table_suffix;
+		$table_suffix = "sociallink";
+		$table_name = $wpdb->prefix . $table_suffix;
+		$edit_id = "";
+		//Edit
+		if (isset($_REQUEST['Submit']) && $_REQUEST['edit_id']!="") {
+			
+			$qry = "UPDATE $table_name SET social_network = '" . $social_network . "',social_link_url='" . $social_link_url . "', img_link='" . $img_link . "' WHERE id = " . $_REQUEST['edit_id'];
+			$wpdb->query($qry);
+			echo "<div id=\"message\" class=\"updated fade\"><p><strong>Social Link Updated Successfully!</strong></p></div>";
+		}
+		// Insert
+		if (isset($_REQUEST['Submit']) && $_REQUEST['edit_id']==""  ) {
+			$rows_affected = $wpdb->insert( $table_name, array( 'social_network' => $social_network, 'social_link_url' => $social_link_url, 'img_link' => $img_link) );
+			echo "<div id=\"message\" class=\"updated fade\"><p><strong>Social Link Inserted Successfully!</strong></p></div>";
+		}
+		//delete
+		if ($_GET["action"]=="delete")
+		{
+			$wpdb->query("DELETE FROM $table_name WHERE id = " .$_GET["social_link"]);
+			echo "<div id=\"message\" class=\"updated fade\"><p><strong>Social Link Deleted Successfully!</strong></p></div>";
+		}
+		//edit
+		if ($_GET["action"]=="edit")
+		{
+			$edit_id = $_GET["social_link"];
+			$edit_data = $wpdb->get_row("SELECT * FROM $table_name where id = $edit_id");
+			$edit_social_network = $edit_data->social_network;
+			$edit_social_link_url = $edit_data->social_link_url;
+			$edit_img_link = $edit_data->img_link;
+			
+		}
+		//is display option set
+		if (isset($_REQUEST['DisplayOption']))
+		{
+			$settings = $_REQUEST['display_option'];
+			set_option('st_social_link_header',$settings);
+		}
+		//Refresh Table
+		$SocialLinkListTable = new Social_Link_List_Table();
+		$SocialLinkListTable->prepare_items(); 
+	?>
+	
+	<div class="wrap">
+		<h2>Social Plugin</h2>
+			<div class="postbox-container" style="width:70%;padding-right:20px;">
+				<div class="metabox-holder">
+					<div class="meta-box-sortables">
+						<div class="postbox"><div class="handlediv" title="Click to toggle"><br/></div>
+							<div class="meta-box-sortables ui-sortable">
+								<h3 class="hndle"><span>Add / Edit</span></h3>
+								<div class="inside">
+									<form id="add_social_link" action="<?php echo $_SERVER['PHP_SELF']."?page=social-link"; ?>" method="post">
+										<input type="hidden" name="edit_id" value="<?=$edit_id?>" />	
+										<select id="social_network" name="social_network" onchange="showimages();">
+											<option value="" ></option>
+											<?php 
+												global $networks;
+												foreach($networks as $k => $v)
+												{
+													$selected = '';
+													if ($_GET["action"]=="edit" && $edit_social_network == $k) 
+													{$selected = 'selected="selected"';} 
+													echo "<option value='" .$k. "' $selected >".$v."</option>";
+												}
+											?>
+										</select>
+										<label>Link</label>
+										<input type="text" name="social_link_url" class="regular-text" value="<?=$edit_social_link_url?>"/>
+										<?php
+											global $networks;
+											global $icon_images;
+											foreach($networks as $k => $v)
+											{
+												$style = 'style="display:none;"';
+												if ($_GET["action"]=="edit" && $edit_social_network == $k) 
+												{$style = 'style="display:block;"';}
+												echo "<div id='$k' class='icon_all_img' $style>";
+													foreach($icon_images[$k] as $icon)
+													{
+														$checked = '';
+														if ($_GET["action"]=="edit" && strpos($edit_img_link,$icon) > 0) 
+														{$checked = 'checked="checked"';} 
+														echo "<div class='icon_image'>";
+														echo "<input type='radio' name='img_link' value='$icon' $checked>";
+														echo "<img src=" . plugins_url('images/' . $icon .'.png',__FILE__). " \>";
+														echo "</div>";
+													}
+												echo "</div>";
+											}
+										?>
+										
+										<input class="button-primary" type="submit" name="Submit" value="Submit" />
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="postbox"><div class="handlediv" title="Click to toggle"><br/></div>
+							<div class="meta-box-sortables ui-sortable">
+								<h3 class="hndle"><span>Links</span></h3>
+									<div class="inside">
+										<form id="link_table" method="get">
+											<input type="hidden" name="page" value="<?=$_REQUEST['page'];?>" />
+	<?php	echo $SocialLinkListTable->display(); ?>
+										</form>
+									</div>
+								</div>
+						</div>
+					</div>		
+				</div>	
+			</div>
+			
+			<div class="postbox-container side" style="width:20%;">
+				<div class="metabox-holder">
+					<div class="meta-box-sortables">	
+						<div id="toc" class="postbox">
+							<div class="handlediv" title="Click to toggle"><br /></div>
+							<h3 class="hndle"><span>How to Use</span></h3>
+							<div class="inside">
+								<ol>
+									<li><strong>Add Social Links</strong></li>
+									<li><strong>Add Social Link Widget</strong></li>
+									or
+									<li><strong>Use Short Code <code>[stsociallink]</code></strong></li>
+									or
+									<li><strong>Use PHP Code </strong><br/>
+										<code>
+											&lt;?php
+											if (function_exists('add_social_link')) {
+												print add_social_link('[stsociallink]');
+											}
+											?&gt;
+										</code>
+									</li>
+								</ol>
+							</div>
+						</div>
+						<div id="toc" class="postbox">
+							<div class="handlediv" title="Click to toggle"><br /></div>
+							<h3 class="hndle"><span>Show your Support</span></h3>
+							<div class="inside">
+								<p><strong>Want to help make this plugin even better? All donations are used to improve this plugin, so donate now!</strong></p>
+								<a href="http://sanskrutitech.in/wordpress-plugins/st-social-links/">Donate</a>
+								<p>Or you could:</p>
+								<ul>
+									<li><a href="http://wordpress.org/extend/plugins/st-social-links/">Rate the plugin 5 star on WordPress.org</a></li>
+									<li><a href="http://wordpress.org/support/plugin/st-social-links">Help out other users in the forums</a></li>
+									<li>Blog about it &amp; link to the <a href="http://sanskrutitech.in/wordpress-plugins/st-social-links/">plugin page</a></li>
+								</ul>
+							</div>
+						</div>
+						<div id="toc" class="postbox">
+							<div class="handlediv" title="Click to toggle"><br/></div>
+							<h3 class="hndle"><span>Connect With Us </span></h3>
+							<div class="inside">
+								<a class="facebook" href="https://www.facebook.com/sanskrutitech"></a>
+								<a class="twitter" href="https://twitter.com/#!/sanskrutitech"></a>
+								<a class="googleplus" href="https://plus.google.com/107541175744077337034/posts"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+	<?php
+}
+?>
---
+++ st_social_links.php
@@ -0,0 +1,227 @@
+<?php
+/*
+Plugin Name: St Social Links
+Plugin URI: http://sanskrutitech.in/index.php/wordpress-plugins/
+Description: A simple plugin to add links to your social network. You can add the list on header, footer or widget.
+Version: 0.0.5
+Author: Dhara Shah
+Author URI: http://sanskrutitech.in/
+License: GPL
+*/
+
+define('WP_SOCIAL_LINK_FOLDER', dirname(plugin_basename(__FILE__)));
+define('WP_SOCIAL_LINK_URL', plugins_url('',__FILE__));
+
+register_activation_hook(__FILE__,'st_social_install'); 
+
+/* Runs on plugin deactivation*/
+register_deactivation_hook( __FILE__, 'st_social_uninstall' );
+
+global $st_social_link_db_ver;
+global $table_suffix;
+
+$st_social_link_db_ver = "0.0.5";
+$table_suffix = "sociallink";
+
+if ( is_admin() )
+{
+	require_once dirname( __FILE__ ) . '/st_social_admin.php';
+}
+function generate_links()
+{
+	//fetch data from table
+	global $wpdb;
+	global $table_suffix;
+	$table_suffix = "sociallink";
+	$table_name = $wpdb->prefix . $table_suffix;
+	$table_result = $wpdb->get_results("SELECT * FROM $table_name");
+	
+	$links = '';
+	foreach ( $table_result as $table_row ) 
+	{
+		$links = $links .  '<div style="float:left;padding:2px;">';
+		$links = $links .  '<a target="_blank" href="'.$table_row->social_link_url.'" ><img src="'.$table_row->img_link.'" \></a>';
+		$links = $links .  '</div>';
+	}
+	return $links;
+}
+add_shortcode( 'stsociallink', 'add_social_link');
+function add_social_link($attr)
+{
+	return generate_links();	
+}
+function st_social_install()
+{	
+	global $wpdb;
+	global $table_suffix;
+	global $st_daily_tip_db_ver;
+	 
+	$table_name = $wpdb->prefix . $table_suffix;
+	
+	$sql = "CREATE TABLE " . $table_name . " (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			social_network	varchar(50) NOT NULL,
+			social_link_url varchar(255) NOT NULL,
+			img_link		varchar(255) NOT NULL,
+			PRIMARY KEY id (id)
+		);";
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	dbDelta($sql);
+	
+	global $st_facebook,$st_twitter,$st_linkedin,$st_googleplus;
+	
+	$st_facebook=get_option("st_social_facebook");
+	$st_twitter=get_option("st_social_twitter");
+	$st_linkedin=get_option("st_social_linkedin");
+	$st_googleplus=get_option("st_social_googleplus");
+	
+	if($st_facebook!=false){
+		//add_option("st_social_facebook",$st_facebook);
+		if (get_option("st_social_small_imgfb")=="on")
+		{
+			$img_url = plugins_url('images/facebook_30.png',__FILE__);
+		}
+		else if (get_option("st_social_big_imgfb")=="on")
+		{
+			$img_url = plugins_url('images/likefb.png',__FILE__);
+		}		
+		$rows_affected = $wpdb->insert( $table_name, array( 'social_network'=>'facebook','social_link_url' => $st_facebook, 'img_link' => $img_url) );
+		delete_option('st_social_facebook');
+		delete_option('st_social_small_imgfb');
+		delete_option('st_social_big_imgfb');
+	}
+	if($st_twitter!=false){
+		//add_option("st_social_twitter",$st_twitter);
+		if (get_option("st_social_small_imgtwit")=="on")
+		{
+			$img_url = plugins_url('images/twitter_30.png',__FILE__);
+		}
+		if (get_option("st_social_big_imgtwit")=="on")
+		{
+			$img_url = plugins_url('images/followtwitt.png',__FILE__);
+		}
+		$rows_affected = $wpdb->insert( $table_name, array( 'social_network'=>'twitter','social_link_url' => $st_twitter, 'img_link' => $img_url) );
+		delete_option('st_social_twitter');
+		delete_option('st_social_small_imgtwit');
+		delete_option('st_social_big_imgtwit');
+	}
+	if($st_linkedin!=false){
+		//add_option("st_social_linkedin",$st_linkedin);
+		if (get_option("st_social_small_imglinkedin")=="on")
+		{
+			$img_url = plugins_url('images/linkedin-small.png',__FILE__);
+		}
+		if (get_option("st_social_big_imglinkedin")=="on")
+		{
+			$img_url = plugins_url('images/linkedinfollow.png',__FILE__);
+		}
+		$rows_affected = $wpdb->insert( $table_name, array( 'social_network'=>'linkedin','social_link_url' => $st_linkedin, 'img_link' => $img_url) );
+		delete_option('st_social_small_imglinkedin');
+		delete_option('st_social_big_imglinkedin');
+		delete_option('st_social_linkedin');
+	}
+	if($st_googleplus!=false){
+		//add_option("st_social_googleplus",$st_googleplus);
+		if (get_option("st_social_small_imggoogle")=="on")
+		{
+			$img_url = plugins_url('images/googleplus_30.png',__FILE__);
+		}
+		if (get_option("st_social_big_imggoogle")=="on")
+		{
+			$img_url = plugins_url('images/googleplusfollow.png',__FILE__);
+		}
+		$rows_affected = $wpdb->insert($table_name,array('social_network'=>'googleplus','social_link_url'=>$st_googleplus, 'img_link' => $img_url) );
+	}
+	add_option("st_social_link_db_ver", $st_social_link_db_ver);
+	delete_option('st_social_googleplus');
+	delete_option('st_social_small_imggoogle');
+	delete_option('st_social_big_imggoogle');
+}
+
+function st_social_uninstall() {
+
+}
+
+/**
+ * Add function to widgets_init that'll load our widget.
+ */
+add_action( 'widgets_init', 'st_social_load_widgets' );
+
+
+/**
+ * Register our widget.
+ * 'st_social_Widget' is the widget class used below.
+ */
+function st_social_load_widgets() {
+	register_widget( 'st_social_Widget' );
+}
+
+class st_social_Widget extends WP_Widget {
+
+	/**
+	 * Widget setup.
+	 */
+	function st_social_Widget() {
+		/* Widget settings. */
+		$widget_ops = array( 'classname' => 'social_link', 'description' => __('An widget that displays Social Link ', 'social_link') );
+
+		/* Widget control settings. */
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'st-social-widget' );
+
+		/* Create the widget. */
+		$this->WP_Widget( 'st-social-widget', __('Social Links', 'social_link'), $widget_ops, $control_ops );
+	}
+
+	
+	/**
+	 * How to display the widget on the screen.
+	 */
+	function widget( $args, $instance )
+	{
+		extract( $args );
+
+		/* Our variables from the widget settings. */
+		$title = apply_filters('widget_title', $instance['title'] );
+		if ( $title )
+		{
+			echo $before_title . $title . $after_title;
+		}
+		$st_fb_value=get_option("st_social_facebook");
+		$st_twit_value=get_option("st_social_twitter");
+	
+		$links = generate_links();
+		echo '<div>';
+		echo $links;
+		echo '</div>';
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		/* Strip tags for title and name to remove HTML (important for text inputs). */
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		
+		return $instance;
+	}
+	
+	/**
+	 * Displays the widget settings controls on the widget panel.
+	 * Make use of the get_field_id() and get_field_name() function
+	 * when creating your form elements. This handles the confusing stuff.
+	 */
+	function form( $instance ) 
+	{
+		/* Set up some default widget settings. */
+		//$defaults = array( 'title' => __('Social Link', 'social link') );
+		$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+		<!-- Widget Title: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'hybrid'); ?></label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+		</p>
+	<?php
+	}
+}
+
+?>
